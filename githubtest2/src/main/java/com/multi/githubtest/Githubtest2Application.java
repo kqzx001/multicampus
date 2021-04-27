2d9953e60e0d50ccfd21b8ae123eabbc0746bbd8
이현용 <kqzx001@naver.com>
2021-04-27 10:39:57 +0900

    두번쨰 연습용

---
+++ githubtest2/src/main/java/com/multi/githubtest/Githubtest2Application.java
@@ -0,0 +1,13 @@
+package com.multi.githubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githubtest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Githubtest2Application.class, args);
+	}
+
+}
